77b65ce0915dddd84cb1755d9d46094f5fe9869d
Fredi Bach <fredi.bach@unic.com>
2022-02-28 16:35:26 +0100

    Update gitignore and add Bootstrap packages for Bootstrap fieldsets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Subsidies from './Subsidies';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (process.env.REACT_APP_BUILD_TARGET === 'subsidies') {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Subsidies />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+
