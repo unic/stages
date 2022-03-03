96a91f05d2bcf3d4d2c1da7d12c8858e213b9abd
Fredi Bach <fredi.bach@unic.com>
2022-03-03 15:32:04 +0100

    Remove Subsidies Test App

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Subsidies from './Subsidies';
 
-if (process.env.REACT_APP_BUILD_TARGET === 'subsidies') {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Subsidies />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
