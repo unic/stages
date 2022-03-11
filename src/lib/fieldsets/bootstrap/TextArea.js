13c918925665d78472cb377aa31b101156b40825
Fredi Bach <fredi.bach@unic.com>
2022-03-11 09:59:39 +0100

    Add tooltip to textarea

--- src/lib/fieldsets/bootstrap/TextArea.js
+++ src/lib/fieldsets/bootstrap/TextArea.js
@@ -1,5 +1,43 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Button from "react-bootstrap/Button";
+
+const TextAreaTooltip = ({ text }) => {
+    return (
+        <div
+            style={{
+                position: "absolute",
+                content: "",
+                top: "30px",
+                right: "9px"
+            }}
+        >
+            <OverlayTrigger
+                placement="left"
+                overlay={(
+                    <Tooltip>{text}</Tooltip>
+                )}
+            >
+                <Button
+                    variant="secondary"
+                    style={{
+                        borderRadius: "100%",
+                        backgroundColor: "#fff",
+                        border: "1px #333 solid",
+                        fontSize: "11px",
+                        fontWeight: "bold",
+                        color: "#333",
+                        padding: 0,
+                        width: "18px",
+                        height: "18px"
+                    }}
+                >?</Button>
+            </OverlayTrigger>
+        </div>
+    );
+};
 
 /*
 
@@ -32,12 +70,13 @@ const TextArea = ({
     isDisabled,
     prefix,
     suffix,
+    tooltip,
     secondaryText,
     type,
     errorRenderer
 }) => {
     return (
-        <Form.Group className="mb-3" controlId={id}>
+        <Form.Group className="mb-3" controlId={id} style={{ position: "relative" }}>
             {label ? <Form.Label>{label}{isRequired ? " *" : ""}</Form.Label> : null}
             {secondaryText ? <Form.Text className="text-muted">{secondaryText}</Form.Text> : null}
             {prefix ? <span>{prefix}</span> : null}
@@ -52,6 +91,7 @@ const TextArea = ({
                     if (typeof onChange === "function") onChange(e.target.value);
                 }}
             />
+            {tooltip ? <TextAreaTooltip text={tooltip} /> : null}
             {suffix ? <span>{suffix}</span> : null}
             {error ? errorRenderer ? errorRenderer(error) : (
                 <Form.Control.Feedback type="invalid">Please fill out this field!</Form.Control.Feedback>
