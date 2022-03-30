b10590435139caca05c9b728de4692bbbd734ac8
Fredi Bach <fredi.bach@unic.com>
2022-03-30 12:27:20 +0200

    Remove bootstrap fields from the distribution, it should be loaded manually

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,4 +7,3 @@ export { default as Form } from "./form/Form";
 export { default as Actions } from "./form/Actions";
 
 export { default as plainFields } from "./fieldsets/plain";
-export { default as bootstrapFields } from "./fieldsets/bootstrap";
