3728bd8e6361f8431c8f1a5e684553f4f1763504
Fredi Bach <fredi.bach@unic.com>
2022-06-24 10:11:44 +0200

    Add new debugger to demos

--- demo/components/Layout.jsx
+++ demo/components/Layout.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import DemoNav from "./DemoNav";
-import DemoDebugger from "./DemoDebugger";
+import { Debugger } from "react-stages";
 
 const Layout = ({ children }) => {
     return (
@@ -16,16 +16,16 @@ const Layout = ({ children }) => {
             <DemoNav />
             <div
                 style={{
-                    margin: "32px",
+                    margin: "32px 4px 32px 32px",
                     position: "relative"
                 }}
             >
                 <div
                     style={{
                         position: "absolute",
-                        top: "-24px",
-                        right: 0,
-                        width: "280px",
+                        top: "-52px",
+                        right: "8px",
+                        width: "320px",
                         padding: "8px",
                         fontSize: "14px",
                         color: "#944",
@@ -36,7 +36,12 @@ const Layout = ({ children }) => {
                 >
                     <strong>Note:</strong> Stages comes without styles, you have to bring your own!
                 </div>
-                <DemoDebugger />
+                <div
+                style={{
+                    margin: "64px 0 0 0",
+                    position: "relative"
+                }}
+                ><Debugger /></div>
                 <div style={{ maxWidth: "900px" }}>
                     {children}
                 </div>
