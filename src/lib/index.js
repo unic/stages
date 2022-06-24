e88dfbebfbd91c6c6d15c5d2d7c6d983543bc4f9
Fredi Bach <fredi.bach@unic.com>
2022-06-24 09:58:09 +0200

    Add new build

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,7 @@ export { default as Stages } from "./stages/Stages";
 export { default as HashRouter } from "./stages/HashRouter";
 export { default as Navigation } from "./stages/Navigation";
 export { default as Progression } from "./stages/Progression";
+export { default as Debugger } from "./stages/Debugger";
 
 export { default as Form } from "./form/Form";
 export { default as Actions } from "./form/Actions";
