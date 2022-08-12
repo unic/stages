2b6f759e5ebae998b5373e98caeb5d397630b33f
Fredi Bach <fredi.bach@unic.com>
2022-08-12 15:58:08 +0200

    Update demos with latest version of stages

--- demo/pages/validateon.jsx
+++ demo/pages/validateon.jsx
@@ -3,7 +3,10 @@ import { Form, plainFields as fields } from "react-stages";
 import Layout from "../components/Layout";
 
 function FormPage() {
-    const [data, setData] = useState({});
+    const [data1, setData1] = useState({});
+    const [data2, setData2] = useState({});
+    const [data3, setData3] = useState({});
+    const [data4, setData4] = useState({});
     return (
         <Layout>
             <p>
@@ -12,7 +15,8 @@ function FormPage() {
             <br />
             <h3>Default: Validate on Form Button Actions (Submit):</h3>
             <Form
-                data={data}
+                id="form1"
+                data={data1}
                 fields={fields}
                 config={{
                     fields: () => {
@@ -27,7 +31,8 @@ function FormPage() {
                                 id: "email",
                                 label: "Email Validated Field",
                                 type: "email",
-                                isRequired: true
+                                isRequired: true,
+                                customValidation: ({ data, allData, isValid }) => isValid && data.indexOf('@') > -1 && data.indexOf('.') > -1
                             },
                             {
                                 id: "optional",
@@ -56,12 +61,13 @@ function FormPage() {
                         </button>
                     </>
                 )}
-                onChange={payload => setData(payload)}
+                onChange={payload => setData1(payload)}
             />
             <br /><br />
             <h3>Validate on blur (and submit):</h3>
             <Form
-                data={data}
+                id="form2"
+                data={data2}
                 fields={fields}
                 validateOn={["action", "blur"]}
                 config={{
@@ -77,7 +83,8 @@ function FormPage() {
                                 id: "email",
                                 label: "Email Validated Field",
                                 type: "email",
-                                isRequired: true
+                                isRequired: true,
+                                customValidation: ({ data, allData, isValid }) => isValid && data.indexOf('@') > -1 && data.indexOf('.') > -1
                             },
                             {
                                 id: "optional",
@@ -106,12 +113,13 @@ function FormPage() {
                         </button>
                     </>
                 )}
-                onChange={payload => setData(payload)}
+                onChange={payload => setData2(payload)}
             />
             <br /><br />
             <h3>Validate on change (and submit):</h3>
             <Form
-                data={data}
+                id="form3"
+                data={data3}
                 fields={fields}
                 validateOn={["action", "change"]}
                 config={{
@@ -127,7 +135,8 @@ function FormPage() {
                                 id: "email",
                                 label: "Email Validated Field",
                                 type: "email",
-                                isRequired: true
+                                isRequired: true,
+                                customValidation: ({ data, allData, isValid }) => isValid && data.indexOf('@') > -1 && data.indexOf('.') > -1
                             },
                             {
                                 id: "optional",
@@ -156,7 +165,59 @@ function FormPage() {
                         </button>
                     </>
                 )}
-                onChange={payload => setData(payload)}
+                onChange={payload => setData3(payload)}
+            />
+            <br /><br />
+            <h3>Validate on throttled change (and submit):</h3>
+            <Form
+                id="form4"
+                data={data4}
+                fields={fields}
+                validateOn={["action", "throttledChange"]}
+                config={{
+                    fields: () => {
+                        return [
+                            {
+                                id: "required",
+                                label: "Required Field",
+                                type: "text",
+                                isRequired: true
+                            },
+                            {
+                                id: "email",
+                                label: "Email Validated Field",
+                                type: "email",
+                                isRequired: true,
+                                customValidation: ({ data, allData, isValid }) => isValid && data.indexOf('@') > -1 && data.indexOf('.') > -1
+                            },
+                            {
+                                id: "optional",
+                                label: "Optional Field",
+                                type: "text",
+                                isRequired: false
+                            }
+                        ]
+                    }
+                }}
+                render={({ actionProps, fieldProps }) => (
+                    <>
+                        <div className="pure-g">
+                            <div className="pure-u-8-24">{fieldProps.fields.required}</div>
+                            <div className="pure-u-8-24">{fieldProps.fields.email}</div>
+                            <div className="pure-u-8-24">{fieldProps.fields.optional}</div>
+                        </div>
+                        <br />
+                        <hr />
+                        <br />
+                        <button
+                            type="button"
+                            onClick={() => actionProps.handleActionClick(payload => console.log("onSubmit:", payload), true)}
+                        >
+                            Submit
+                        </button>
+                    </>
+                )}
+                onChange={payload => setData4(payload)}
             />
         </Layout>
     );
