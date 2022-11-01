ec583f9ca3bb94dd8834fa981645f8b21640c528
Fredi Bach <fredi.bach@unic.com>
2022-11-01 14:33:20 +0100

    Add demo for deeply nested groups

--- demo/pages/groups.jsx
+++ demo/pages/groups.jsx
@@ -48,6 +48,48 @@ function FormPage() {
                                         ]
                                     }
                                 ]
+                            },
+                            {
+                                id: "deepcollection",
+                                type: "collection",
+                                fields: [
+                                    {
+                                        id: "field1",
+                                        label: "Field 1",
+                                        type: "text",
+                                        isRequired: true
+                                    },
+                                    {
+                                        id: "field2",
+                                        label: "Field 2",
+                                        type: "text",
+                                        isRequired: false
+                                    },
+                                    {
+                                        id: "colGroup",
+                                        type: "group",
+                                        fields: [
+                                            {
+                                                id: "field",
+                                                label: "Field",
+                                                type: "text",
+                                                isRequired: true
+                                            },
+                                            {
+                                                id: "subcollection",
+                                                type: "collection",
+                                                fields: [
+                                                    {
+                                                        id: "field",
+                                                        label: "Field",
+                                                        type: "text",
+                                                        isRequired: true
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                ]
                             }
                         ]                        
                     }
@@ -69,6 +111,41 @@ function FormPage() {
                                     <br />
                                     {fieldProps.fields.mygroup.mysubgroup.field2}
                                 </fieldset>
+                            </fieldset>
+                        </div>
+                        <h3>Deeply nested groups and collections</h3>
+                        <p>An example with a group inside of a collection, which contains another collection. Start adding entries to see the behaviour:</p>
+                        <div>
+                            <fieldset>
+                                {fieldProps.fields.deepcollection ? fieldProps.fields.deepcollection.map((subFields, index) => (
+                                    <div key={`deepcollection-${index}`} style={{ background: "#eee", margin: "8px", padding: "8px" }}>
+                                        <div className="pure-g">
+                                            <div className="pure-u-1-3">{subFields.field1}</div>
+                                            <div className="pure-u-1-3">{subFields.field2}</div>
+                                        </div>
+                                        <br />
+                                        <div className="pure-g">
+                                            <fieldset>
+                                                <div className="pure-u-1-3">{subFields.colGroup.field}</div>
+                                                <br /><br />
+                                                <fieldset>
+                                                    {subFields.colGroup.subcollection ? subFields.colGroup.subcollection.map((subSubFields, subIndex) => (
+                                                        <div key={`subcollection-${subIndex}`} style={{ background: "#eee", margin: "8px", padding: "8px" }}>
+                                                            <div className="pure-g">
+                                                                <div className="pure-u-1-3">{subSubFields.field}</div>
+                                                            </div>
+                                                            <br />
+                                                            <button type="button" onClick={() => fieldProps.onCollectionAction(`deepcollection[${index}].colGroup.subcollection`, "remove", subIndex)}>-</button>
+                                                        </div>)
+                                                    ) : null}
+                                                    <button type="button" onClick={() => fieldProps.onCollectionAction(`deepcollection[${index}].colGroup.subcollection`, "add")}>+</button>
+                                                </fieldset>
+                                            </fieldset>
+                                        </div>
+                                        <button type="button" onClick={() => fieldProps.onCollectionAction("deepcollection", "remove", index)}>-</button>
+                                    </div>)
+                                ) : null}
+                                <button type="button" onClick={() => fieldProps.onCollectionAction("deepcollection", "add")}>+</button>
                             </fieldset>
                         </div>
                         <br />
