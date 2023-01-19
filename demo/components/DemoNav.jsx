e3a7d472bb40a9b2b4fb8b11ce56c01de1abb970
Fredi Bach <fredi.bach@unic.com>
2023-01-19 18:29:28 +0100

    Improve demo styles and layout

--- demo/components/DemoNav.jsx
+++ demo/components/DemoNav.jsx
@@ -1,200 +1,75 @@
 import React from "react";
-import Link from 'next/link';
 import { useRouter } from "next/router";
-
-const StyledLi = ({ children }) => {
-    return (
-        <li
-            style={{
-                padding: "4px 8px",
-                margin: "8px 0 0 8px",
-                whiteSpace: "nowrap",
-                background: "#555"
-            }}
-        >
-            {children}
-        </li>
-    );
-};
-
-const MenuItem = React.forwardRef(({ onClick, href, children, active }, ref) => {
-    return (
-        <a href={href} onClick={onClick} ref={ref} style={{
-            textDecoration: "none",
-            color: active ? "#fff" : "#ccc",
-            cursor: "pointer"
-        }}>{children}</a>
-    );
-});
+import { Debugger } from "react-stages";
 
 const DemoNav = () => {
     const router = useRouter();
 
     return (
-        <div
-            style={{
-                background: "#333",
-                color: "#eee",
-                padding: 0,
-                marginBottom: "32px",
-                fontFamily: "Helvetica, Arial, Sans"
-            }}
-        >
-            <h1
+        <div style={{ display: "flex", borderBottom: "1px #ccc dashed", padding: "6px 0", background: "#f8f8f8" }}>
+            <div>
+                <h1
+                    style={{
+                        float: "left",
+                        padding: "8px 4px 6px 8px",
+                        fontSize: "18px",
+                        lineHeight: "14px",
+                        height: "100%",
+                        margin: "8px"
+                    }}
+                >Stages Demos:</h1>
+                <select onChange={e => router.push(e.target.value)} style={{ marginTop: "12px" }} defaultValue={router.pathname}>
+                    <optgroup label="Form">
+                        <option value="/">Simple Form</option>
+                        <option value="/disabled">Disabled Form</option>
+                        <option value="/asyncdata">Async Data</option>
+                        <option value="/validateon">Validate On</option>
+                        <option value="/isdirty">Is Dirty</option>
+                        <option value="/undo">Undo / Redo</option>
+                        <option value="/interfacestate">Interface State</option>
+                        <option value="/subform">Subform</option>
+                        <option value="/configtemplates">Config Templates</option>
+                        <option value="/i18n">i18n</option>
+                    </optgroup>
+                    <optgroup label="Fields">
+                        <option value="/dynamicfields">Dynamic Fields</option>
+                        <option value="/dynamicoptions">Dynamic Options</option>
+                        <option value="/computedoptions">Computed Options</option>
+                        <option value="/dynamicvalues">Dynamic Values</option>
+                        <option value="/groups">Groups</option>
+                        <option value="/customerrors">Custom Errors</option>
+                        <option value="/typecasting">Typecasting</option>
+                        <option value="/wysiwyg">WYSIWYG Editor</option>
+                    </optgroup>
+                    <optgroup label="Collections">
+                        <option value="/collections">Collections</option>
+                        <option value="/collectionrules">Collection Rules</option>
+                    </optgroup>
+                    <optgroup label="Wizard">
+                       <option value="/wizard">Simple Wizard</option>
+                       <option value="/dynamicsteps">Dynamic Steps</option>
+                       <option value="/stepsummaries">Step Summaries</option>
+                       <option value="/wizardnavigation">Wizard Navigation</option>
+                    </optgroup>
+                    <optgroup label="Various">
+                        <option value="/slideshow">Slideshow</option>
+                        <option value="/quiz">Quiz</option>
+                        <option value="/sparouter">SPA Router</option>
+                    </optgroup>
+                </select>
+            </div>
+            <div
                 style={{
-                    float: "left",
-                    padding: "5px 14px 6px 16px",
-                    fontSize: "16px",
-                    lineHeight: "14px",
-                    height: "100%",
-                    background: "#d30",
-                    margin: "8px"
+                    fontSize: "14px",
+                    color: "#c03",
+                    padding: "7px 32px"
                 }}
-            >Stages Demos:</h1>
-            <ul style={{
-                listStyleType: "none",
-                display: "flex",
-                flexWrap: "wrap",
-                margin: 0,
-                padding: "0 0 8px 0",
-                lineHeight: "1"
-            }}>
-                <StyledLi>
-                    <Link href="/">
-                        <MenuItem active={router.pathname === "/"}>Simple Form</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/disabled">
-                        <MenuItem active={router.pathname === "/disabled"}>Disabled Form</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/dynamicfields">
-                        <MenuItem active={router.pathname === "/dynamicfields"}>Dynamic Fields</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/dynamicoptions">
-                        <MenuItem active={router.pathname === "/dynamicoptions"}>Dynamic Options</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/computedoptions">
-                        <MenuItem active={router.pathname === "/computedoptions"}>Computed Options</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/dynamicvalues">
-                        <MenuItem active={router.pathname === "/dynamicvalues"}>Dynamic Values</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/collections">
-                        <MenuItem active={router.pathname === "/collections"}>Collections</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/collectionrules">
-                        <MenuItem active={router.pathname === "/collectionrules"}>Collection Rules</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/groups">
-                        <MenuItem active={router.pathname === "/groups"}>Groups</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/asyncdata">
-                        <MenuItem active={router.pathname === "/asyncdata"}>Async Data</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/customerrors">
-                        <MenuItem active={router.pathname === "/customerrors"}>Custom Errors</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/validateon">
-                        <MenuItem active={router.pathname === "/validateon"}>Validate On</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/isdirty">
-                        <MenuItem active={router.pathname === "/isdirty"}>Is Dirty</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/undo">
-                        <MenuItem active={router.pathname === "/undo"}>Undo / Redo</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/interfacestate">
-                        <MenuItem active={router.pathname === "/interfacestate"}>Interface State</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/typecasting">
-                        <MenuItem active={router.pathname === "/typecasting"}>Typecasting</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/subform">
-                        <MenuItem active={router.pathname === "/subform"}>Subform</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/wizard">
-                        <MenuItem  active={router.pathname === "/wizard"}>Simple Wizard</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/dynamicsteps">
-                        <MenuItem  active={router.pathname === "/dynamicsteps"}>Dynamic Steps</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/configtemplates">
-                        <MenuItem  active={router.pathname === "/configtemplates"}>Config Templates</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/i18n">
-                        <MenuItem  active={router.pathname === "/i18n"}>i18n</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/stepsummaries">
-                        <MenuItem  active={router.pathname === "/stepsummaries"}>Step Summaries</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/wizardnavigation">
-                        <MenuItem  active={router.pathname === "/wizardnavigation"}>Wizard Navigation</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/slideshow">
-                        <MenuItem active={router.pathname === "/slideshow"}>Simple Slideshow</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/quiz">
-                        <MenuItem active={router.pathname === "/quiz"}>Quiz</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/sparouter">
-                        <MenuItem active={router.pathname === "/sparouter"}>SPA Router</MenuItem>
-                    </Link>
-                </StyledLi>
-                <StyledLi>
-                    <Link href="/wysiwyg">
-                        <MenuItem active={router.pathname === "/wysiwyg"}>WYSIWYG</MenuItem>
-                    </Link>
-                </StyledLi>
-            </ul>  
+            >
+                <span style={{ fontSize: "24px", verticalAlign: "-1px" }}>[</span> <strong>Note:</strong> Stages comes without styles, you have to bring your own! <span style={{ fontSize: "24px", verticalAlign: "-1px" }}>]</span>
+            </div>
+            <div>
+                <Debugger />
+            </div>
         </div>
     );
 };
--- demo/components/Layout.jsx
+++ demo/components/Layout.jsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import DemoNav from "./DemoNav";
-import { Debugger } from "react-stages";
 
 const Layout = ({ children }) => {
     return (
@@ -20,28 +19,6 @@ const Layout = ({ children }) => {
                     position: "relative"
                 }}
             >
-                <div
-                    style={{
-                        position: "absolute",
-                        top: "-52px",
-                        right: "8px",
-                        width: "320px",
-                        padding: "8px",
-                        fontSize: "14px",
-                        color: "#944",
-                        border: "1px #baa solid",
-                        borderRadius: "4px",
-                        background: "#fee"
-                    }}
-                >
-                    <strong>Note:</strong> Stages comes without styles, you have to bring your own!
-                </div>
-                <div
-                style={{
-                    margin: "64px 0 0 0",
-                    position: "relative"
-                }}
-                ><Debugger /></div>
                 <div style={{ maxWidth: "960px" }}>
                     {children}
                 </div>
