a52e0c63a6a1f94efa9a0b9dfce875319be95a43
Fredi Bach <fredi.bach@unic.com>
2023-06-22 15:49:20 +0200

    Fix Navigation component types

--- src/lib/stages/Navigation.jsx
+++ src/lib/stages/Navigation.jsx
@@ -5,14 +5,15 @@ import React from "react";
  * a sample on how to build your own, but of course you can use it if it
  * fits your usecase.
  *
- * @param {number} currentStep - The current step of the navigation.
- * @param {Object} data - Current data in the wizard, can be used to display step summaries.
- * @param {function} onChangeStep - Function to call when the step should change.
- * @param {Object} errors - Object containing error data for each step.
- * @param {number} lastValidStep - The last valid step in the wizard.
- * @param {array<object>} keys - All the step keys in the wizard.
- * @param {number} stepCount - The number of steps in the wizard.
- * @param {function} reset - Function to call when the navigation is reset.
+ * @param {Object} props - The component props.
+ * @param {number} props.currentStep - The current step of the navigation.
+ * @param {Object} props.data - Current data in the wizard, can be used to display step summaries.
+ * @param {function} props.onChangeStep - Function to call when the step should change.
+ * @param {Object} props.errors - Object containing error data for each step.
+ * @param {number} props.lastValidStep - The last valid step in the wizard.
+ * @param {Array<object>} props.keys - All the step keys in the wizard.
+ * @param {number} props.stepCount - The number of steps in the wizard.
+ * @param {function} props.reset - Function to call when the navigation is reset.
  * @return {JSX.Element} - The navigation component.
  */
 const Navigation = ({ currentStep, data, onChangeStep, errors, lastValidStep, keys, stepCount, reset }) => {
