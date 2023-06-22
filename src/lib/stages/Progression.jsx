75c539eb071697dd5fee59125990668a7e84fa2b
Fredi Bach <fredi.bach@unic.com>
2023-06-22 15:50:08 +0200

    Fix Progression component types

--- src/lib/stages/Progression.jsx
+++ src/lib/stages/Progression.jsx
@@ -5,9 +5,10 @@ import React from "react";
  * a sample on how to build your own, but of course you can use it if it
  * fits your usecase.
  *
- * @param {number} stepCount - the total number of steps
- * @param {number} validSteps - the number of valid steps
- * @param {number} percentage - the percentage of valid steps
+ * @param {Object} props - The component props.
+ * @param {number} props.stepCount - the total number of steps
+ * @param {number} props.validSteps - the number of valid steps
+ * @param {number} props.percentage - the percentage of valid steps
  * @return {JSX.Element} - a React component displaying the valid steps, the total steps, and the percentage of valid steps
  */
 const Progression = ({ stepCount, validSteps, percentage }) => {
