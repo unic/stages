a18a00fd86789780765e5e85be105543ea561f01
Fredi Bach <fredi.bach@unic.com>
2023-07-28 14:39:01 +0200

    Add page title and description

--- studio/pages/_document.js
+++ studio/pages/_document.js
@@ -24,17 +24,27 @@ export default class AppDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest"></link>
-      </Head>
+            <title>Stages Studio - Form Management System (FMS)</title>
+            <meta name="title" content="Stages Studio - Form Management System (FMS)"/>
+            <meta itemProp="name" content="Stages Studio - Form Management System (FMS)"/>
+            <meta name="twitter:title" content="Stages Studio - Form Management System (FMS)"/>
+            <meta property="og:title" content="Stages Studio - Form Management System (FMS)"/>
+            <meta name="description" content="With Stages Studio you can build forms of any complexity. From the simplest non-structured forms to highly complex, dynamic wizards."/>
+            <meta itemProp="description" content="With Stages Studio you can build forms of any complexity. From the simplest non-structured forms to highly complex, dynamic wizards."/>
+            <meta name="twitter:description" content="With Stages Studio you can build forms of any complexity. From the simplest non-structured forms to highly complex, dynamic wizards."/>
+            <meta property="og:description" content="With Stages Studio you can build forms of any complexity. From the simplest non-structured forms to highly complex, dynamic wizards."/>
+
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+            />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest"></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
