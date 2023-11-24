dced6110fea8d8424147885702da607b25764428
Fredi Bach <fredi.bach@unic.com>
2023-11-24 13:59:31 +0100

    Implement removing a field with context menu

--- studio/pages/c/[communitySlug]/f/[formSlug].jsx
+++ studio/pages/c/[communitySlug]/f/[formSlug].jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, isValidElement } from 'react';
+import { useState, useEffect, useRef, isValidElement } from 'react';
 import { useRouter } from 'next/router';
 import { Form } from "react-stages";
 import { ScrollPanel } from 'primereact/scrollpanel';
@@ -6,6 +6,8 @@ import primeFields from '../../../../components/primeFields';
 import set from "lodash.set";
 import get from "lodash.get";
 import findIndex from "lodash.findindex";
+import unset from "lodash.unset";
+import { ContextMenu } from 'primereact/contextmenu';
 
 const globalFieldProps = {
     id: {
@@ -306,15 +308,15 @@ const fieldProps = {
     ],
 };
 
-const renderFields = (setSelectedElement, isEditMode, selectedElement, fieldProps, fields, type = "field") => {
+const renderFields = (setActiveContextMenuInput, contextMenuRef, setSelectedElement, isEditMode, selectedElement, fieldProps, fields, type = "field") => {
     if (typeof fields !== "object") return null;
     return (
         <>
             {Object.keys(fields).map(key => {
                 const field = fields[key];
                 if (isValidElement(field)) {
-                    if (type === "group") return <EditableBlock setSelectedElement={setSelectedElement} inGroup field={field} path={field.key} isEditMode={isEditMode} selectedElement={selectedElement} />;
-                    return <EditableBlock setSelectedElement={setSelectedElement} field={field} path={field.key} isEditMode={isEditMode} selectedElement={selectedElement} />;
+                    if (type === "group") return <EditableBlock setActiveContextMenuInput={setActiveContextMenuInput} contextMenuRef={contextMenuRef} setSelectedElement={setSelectedElement} inGroup field={field} path={field.key} isEditMode={isEditMode} selectedElement={selectedElement} />;
+                    return <EditableBlock setActiveContextMenuInput={setActiveContextMenuInput} contextMenuRef={contextMenuRef} setSelectedElement={setSelectedElement} field={field} path={field.key} isEditMode={isEditMode} selectedElement={selectedElement} />;
                 } else if (typeof field === "object") {
                     if (Array.isArray(field)) {
                         // collection array
@@ -322,7 +324,7 @@ const renderFields = (setSelectedElement, isEditMode, selectedElement, fieldProp
                             <div style={{ margin: "16px 0 32px 0" }}>
                                 {field.map((entry, index) => (
                                     <div className="flex">
-                                        {renderFields(setSelectedElement, isEditMode, selectedElement, fieldProps, entry, "group")}
+                                        {renderFields(setActiveContextMenuInput, contextMenuRef, setSelectedElement, isEditMode, selectedElement, fieldProps, entry, "group")}
                                         <div className="flex-1" style={{ marginTop: "32px" }}>
                                             <button type="button" onClick={() => fieldProps.onCollectionAction(key, "remove", index)}>remove</button>
                                         </div>
@@ -332,7 +334,7 @@ const renderFields = (setSelectedElement, isEditMode, selectedElement, fieldProp
                             </div>
                         );
                     } else {
-                        return <div className="flex">{renderFields(setSelectedElement, isEditMode, selectedElement, fieldProps, field, "group")}</div>;
+                        return <div className="flex">{renderFields(setActiveContextMenuInput, contextMenuRef, setSelectedElement, isEditMode, selectedElement, fieldProps, field, "group")}</div>;
                     }
                 }
             })}
@@ -340,7 +342,7 @@ const renderFields = (setSelectedElement, isEditMode, selectedElement, fieldProp
     );
 };
 
-const EditableBlock = ({ field, path, isEditMode, selectedElement, inGroup, setSelectedElement }) => {
+const EditableBlock = ({ field, path, isEditMode, selectedElement, inGroup, setSelectedElement, contextMenuRef, setActiveContextMenuInput }) => {
     const [isInEditMode, setIsInEditMode] = useState(isEditMode && selectedElement === path);
 
     return (
@@ -349,6 +351,11 @@ const EditableBlock = ({ field, path, isEditMode, selectedElement, inGroup, setS
             borderRadius: "5px",
             border: isInEditMode && isEditMode ? "1px dashed #0A94F8" : "1px solid rgba(0,0,0,0)",
             position: "relative"
+        }} onContextMenu={(e) => {
+            if (contextMenuRef && contextMenuRef.current) {
+                contextMenuRef.current.show(e);
+                setActiveContextMenuInput(path);
+            }
         }} onMouseOver={() => setIsInEditMode(isEditMode ? true : false)} onMouseOut={() => setIsInEditMode(selectedElement === path ? true : false)}>
             {isInEditMode && isEditMode ? <button style={{ position: "absolute", top: "4px", right: "4px" }} type="button" onClick={() => setSelectedElement(path)}>edit</button> : null}
             {field}
@@ -356,7 +363,7 @@ const EditableBlock = ({ field, path, isEditMode, selectedElement, inGroup, setS
     );
 };
 
-const FiledConfigEditor = ({ path, config, handleEditFieldConfig }) => {
+const FieldConfigEditor = ({ path, config, handleEditFieldConfig }) => {
     const [data, setData] = useState(config);
 
     useEffect(() => {
@@ -383,7 +390,7 @@ const FiledConfigEditor = ({ path, config, handleEditFieldConfig }) => {
                             <>
                                 <form>
                                     <div style={{ position: "relative" }}>
-                                        {renderFields(() => {}, false, '', fieldProps, fieldProps.fields)}
+                                        {renderFields(() => {}, null, () => {}, false, '', fieldProps, fieldProps.fields)}
                                     </div>
                                 </form>
                             </>
@@ -404,8 +411,10 @@ const CommunityForm = () => {
       query: { communitySlug, formSlug },
     } = useRouter();
     const [data, setData] = useState({});
+    const contextMenuRef = useRef(null);
     const [isEditMode, setIsEditMode] = useState(false);
     const [selectedElement, setSelectedElement] = useState('');
+    const [activeContextMenuInput, setActiveContextMenuInput] = useState('');
     const [currentConfig, setCurrentConfig] = useState([
         {
             id: "textgroup",
@@ -558,6 +567,26 @@ const CommunityForm = () => {
         }
     ]);
 
+    const contextMenuItems = [
+        { label: 'Delete', icon: 'pi pi-fw pi-trash', command: () => handleRemoveField(activeContextMenuInput) },
+    ];
+
+    const handleRemoveField = (path) => {
+        const newConfig = [...currentConfig];
+        const pathSplit = path.split(".");
+        let realPath = '';
+        pathSplit.forEach((key) => {
+            const index = findIndex(realPath ? get(newConfig, realPath) : newConfig, { id: key.replace(/\[(\d+)\]/, "") });
+            if (index > -1) {
+                realPath = realPath === "" ? `[${index}]` : `${realPath}[${index}]`;
+                const thisConfig = get(newConfig, realPath);
+                if (thisConfig.fields) realPath += ".fields";
+            }
+        });
+        unset(newConfig, realPath);
+        setCurrentConfig(newConfig);
+    };
+
     const handleEditFieldConfig = (path, config) => {
         const newConfig = [...currentConfig];
         const pathSplit = path.split(".");
@@ -581,6 +610,7 @@ const CommunityForm = () => {
     return (
         <div style={{ marginRight: "350px" }}>
             <h2>Community "{communitySlug}" - Form "{formSlug}"</h2>
+            {isEditMode ? <ContextMenu model={contextMenuItems} ref={contextMenuRef} breakpoint="767px" /> : null}
             {isEditMode ? <button type="button" onClick={() => setIsEditMode(false)}>Preview</button> : <button type="button" onClick={() => setIsEditMode(true)}>Edit</button>}
             <Form
                 id="myForm"
@@ -596,13 +626,13 @@ const CommunityForm = () => {
                         <>
                             <form>
                                 <div style={{ position: "relative", maxWidth: "800px", margin: "0 auto" }}>
-                                    {renderFields(setSelectedElement, isEditMode, selectedElement, fieldProps, fieldProps.fields)}
+                                    {renderFields(setActiveContextMenuInput, contextMenuRef, setSelectedElement, isEditMode, selectedElement, fieldProps, fieldProps.fields)}
                                 </div>
                             </form>
                             {isEditMode ? (
                                 <ScrollPanel style={{ width: '350px', height: '100vh', position: "fixed", top: 0, right: 0, borderLeft: "1px solid #ccc", padding: "12px" }}>
                                     <h3>Inspector:</h3>
-                                    {selectedElement ? <FiledConfigEditor path={selectedElement} config={fieldProps.getConfig(selectedElement)} handleEditFieldConfig={handleEditFieldConfig} /> : null}
+                                    {selectedElement ? <FieldConfigEditor path={selectedElement} config={fieldProps.getConfig(selectedElement)} handleEditFieldConfig={handleEditFieldConfig} /> : null}
                                 </ScrollPanel> 
                             ) : null}
                          </>
