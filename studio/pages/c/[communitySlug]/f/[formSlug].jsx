9385e2d5bc21c9683d96c8dc3126667cf8248033
Fredi Bach <fredi.bach@unic.com>
2023-11-28 12:37:31 +0100

    Add config export

--- studio/pages/c/[communitySlug]/f/[formSlug].jsx
+++ studio/pages/c/[communitySlug]/f/[formSlug].jsx
@@ -392,11 +392,44 @@ const CommunityForm = () => {
         setSelectedElement(path);
     };
 
+    const downloadFile = ({ data, fileName, fileType }) => {
+        // Create a blob with the data we want to download as a file
+        const blob = new Blob([data], { type: fileType });
+        // Create an anchor element and dispatch a click event on it
+        // to trigger a download
+        const a = document.createElement('a');
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+    }
+
+    const exportToJson = e => {
+        e.preventDefault();
+        downloadFile({
+            data: JSON.stringify(currentConfig),
+            fileName: 'stages-config.json',
+            fileType: 'text/json',
+        })
+    }
+
     return (
         <div style={{ marginRight: "350px" }}>
             <h2>Community "{communitySlug}" - Form "{formSlug}"</h2>
             {isEditMode ? <ContextMenu model={activeContextMenuInput.startsWith("insert > ") ? insertContextMenuItems : fieldContextMenuItems} ref={contextMenuRef} breakpoint="767px" /> : null}
             {isEditMode ? <button type="button" onClick={() => setIsEditMode(false)}>Preview</button> : <button type="button" onClick={() => setIsEditMode(true)}>Edit</button>}
+            {isEditMode ? (
+                <div>
+                    <button type='button' onClick={exportToJson}>
+                        Export Config
+                    </button>
+                </div>
+            ) : null}
             {!isEditMode ? <div><br /></div> : null}
             <Form
                 id="myForm"
