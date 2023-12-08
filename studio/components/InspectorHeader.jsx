ab8a66405b1b3511f8513ff89d33e938fdbce66d
Fredi Bach <fredi.bach@unic.com>
2023-12-08 14:12:53 +0100

    Add inspector search box

--- studio/components/InspectorHeader.jsx
+++ studio/components/InspectorHeader.jsx
@@ -1,5 +1,8 @@
 import useStagesStore from './store';
 import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { Search } from 'lucide-react';
+import { Filter } from 'lucide-react';
 
 const GeneralConfig = () => {
     const store = useStagesStore();
@@ -16,6 +19,17 @@ const GeneralConfig = () => {
                 </div>
             </div>
             <div><span style={{ color: "#5AA510" }}>fredi-bach</span>.<span style={{ color: "#C10B99" }}>{store.generalConfig.slug}</span></div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                    <span className="p-input-icon-right" style={{ margin: "16px 0" }}>
+                        <Search color="#999999" size={16} />
+                        <InputText placeholder="stepkey.fieldkey" />
+                    </span>
+                </div>
+                <div style={{ paddingTop: "24px" }}>
+                    <Filter color="#999999" size={20} />
+                </div>
+            </div>
         </div>
     )
 };
