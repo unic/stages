2aaed8567403f8ffdc391d4ba95cd5ebc401e590
Fredi Bach <fredi.bach@unic.com>
2023-12-11 14:53:13 +0100

    Animate in pasted blocks

--- studio/components/EditableBlock.jsx
+++ studio/components/EditableBlock.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { motion } from "framer-motion";
 import useStagesStore from './store';
 import BlockPathLabel from './BlockPathLabel';
 import { pathIsSelected } from './helpers';
@@ -12,31 +13,38 @@ const EditableBlock = ({ field, path, selectedElement, inGroup, contextMenuRef, 
     }, [path, selectedElement]);
 
     return (
-        <div className={inGroup ? "flex-1" : undefined} style={{
-            position: "relative",
-            padding: "8px",
-            borderRadius: "5px",
-            border: isInEditMode && store.isEditMode && !isFieldConfigEditor ? "1px dashed #0A94F8" : !isFieldConfigEditor && store.isEditMode ? "1px dashed #ddd" : "1px solid rgba(0,0,0,0)",
-            maxWidth: inGroup ? "33%" : "100%",
-            background: !isFieldConfigEditor ? "#fff" : "transparent",
-            boxShadow: !isFieldConfigEditor ? "1px 1px 1px 0px rgba(0,0,0,0.05)" : "none"
-        }} onContextMenu={(e) => {
-            if (contextMenuRef && contextMenuRef.current) {
-                contextMenuRef.current.show(e);
-                store.setActiveContextMenuInput(path);
-            }
-        }} onMouseOver={() => setIsInEditMode(store.isEditMode ? true : false)} onMouseOut={() => setIsInEditMode(pathIsSelected(path, selectedElement) ? true : false)}
-        onClick={(e) => {
-            e.stopPropagation();
-            if (isInEditMode && store.isEditMode && !isFieldConfigEditor) {
-                store.setSelectedElement(path, e.shiftKey);
-                store.setEditorTabIndex(1);
-            }
-        }}
+        <motion.div
+            className={inGroup ? "flex-1" : undefined}
+                style={{
+                position: "relative",
+                padding: "8px",
+                borderRadius: "5px",
+                border: isInEditMode && store.isEditMode && !isFieldConfigEditor ? "1px dashed #0A94F8" : !isFieldConfigEditor && store.isEditMode ? "1px dashed #ddd" : "1px solid rgba(0,0,0,0)",
+                maxWidth: inGroup ? "33%" : "100%",
+                background: !isFieldConfigEditor ? "#fff" : "transparent",
+                boxShadow: !isFieldConfigEditor ? "1px 1px 1px 0px rgba(0,0,0,0.05)" : "none"
+            }}
+            onContextMenu={(e) => {
+                if (contextMenuRef && contextMenuRef.current) {
+                    contextMenuRef.current.show(e);
+                    store.setActiveContextMenuInput(path);
+                }
+            }}
+            onMouseOver={() => setIsInEditMode(store.isEditMode ? true : false)} onMouseOut={() => setIsInEditMode(pathIsSelected(path, selectedElement) ? true : false)}
+            onClick={(e) => {
+                e.stopPropagation();
+                if (isInEditMode && store.isEditMode && !isFieldConfigEditor) {
+                    store.setSelectedElement(path, e.shiftKey);
+                    store.setEditorTabIndex(1);
+                }
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
         >
             {store.isEditMode && !isFieldConfigEditor ? <BlockPathLabel path={path} isHovered={isInEditMode} type="field" /> : null}
             {field}
-        </div>
+        </motion.div>
     );
 };
 
