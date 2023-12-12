c00694c433f9ce85dd5d2be79f42ec58b290326a
Fredi Bach <fredi.bach@unic.com>
2023-12-12 09:16:02 +0100

    Add a more realistic initial prop

--- studio/components/initialConfig.js
+++ studio/components/initialConfig.js
@@ -1,153 +1,61 @@
 const initialConfig = [
     {
-        id: "textgroup",
+        id: "username",
+        label: "Username",
+        type: "text",
+        isRequired: true
+    },
+    {
+        id: "email",
+        label: "Email",
+        type: "email",
+        isRequired: true
+    },
+    {
+        id: "passwords",
         type: "group",
+        label: "Passwords",
         fields: [  
             {
-                id: "field1",
-                label: "Text 1",
-                type: "text",
+                id: "password1",
+                label: "Password",
+                type: "password",
                 isRequired: true
             },
             {
-                id: "field2",
-                label: "Text 2",
-                type: "text",
+                id: "password2",
+                label: "Repeat Password",
+                type: "password",
                 isRequired: true
             },
         ]
     },
     {
-        id: "textcollection",
+        id: "hobbies",
         type: "collection",
         min: 1,
         init: true,
         fields: [  
             {
-                id: "field1",
-                label: "Text 1",
+                id: "name",
+                label: "Name",
                 type: "text",
                 isRequired: true
             },
             {
-                id: "field2",
-                label: "Text 2",
-                type: "text",
-                isRequired: true
+                id: "rating",
+                label: "Rating",
+                type: "rating",
+                isRequired: false
             },
         ]
     },
     {
-        id: "field2",
-        label: "Textarea",
+        id: "comments",
+        label: "Comments",
         type: "textarea",
-        isRequired: true
+        isRequired: false
     },
-    {
-        id: "field3",
-        label: "Select",
-        type: "select",
-        options: [
-            {
-                text: "Option 1",
-                value: "option1"
-            },
-            {
-                text: "Option 2",
-                value: "option2"
-            }
-        ],
-        isRequired: true
-    },
-    {
-        id: "field4",
-        label: "Calendar",
-        type: "calendar",
-        isRequired: true
-    },
-    {
-        id: "field5",
-        label: "Checkbox",
-        type: "checkbox",
-        isRequired: true
-    },
-    {
-        id: "field6",
-        label: "Switch",
-        type: "switch",
-        isRequired: true
-    },
-    {
-        id: "field7",
-        label: "Number",
-        type: "number",
-        isRequired: true
-    },
-    {
-        id: "field8",
-        label: "Rating",
-        type: "rating",
-        isRequired: true
-    },
-    {
-        id: "field9",
-        label: "Buttons",
-        type: "buttons",
-        options: [
-            {
-                text: "Option 1",
-                value: "option1"
-            },
-            {
-                text: "Option 2",
-                value: "option2"
-            }
-        ],
-        isRequired: true
-    },
-    {
-        id: "field10",
-        label: "Slider",
-        type: "slider",
-        isRequired: true
-    },
-    {
-        id: "field11",
-        label: "Toggle",
-        type: "toggle",
-        isRequired: true
-    },
-    {
-        id: "field12",
-        label: "Editor",
-        type: "editor",
-        isRequired: true
-    },
-    {
-        id: "field13",
-        label: "Chips",
-        type: "chips",
-        isRequired: true
-    },
-    {
-        id: "field14",
-        label: "Color",
-        type: "color",
-        isRequired: true
-    },
-    {
-        id: "field15",
-        label: "Mask",
-        type: "mask",
-        mask: "99-999999",
-        isRequired: true
-    },
-    {
-        id: "field16",
-        label: "Password",
-        type: "password",
-        isRequired: true
-    }
 ];
 
 export default initialConfig;
