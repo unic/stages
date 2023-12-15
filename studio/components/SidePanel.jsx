054ae55affc1eb39fb8dcc25c3285db97d3aad0b
Fredi Bach <fredi.bach@unic.com>
2023-12-15 11:05:06 +0100

    Small inspector width bugfix

--- studio/components/SidePanel.jsx
+++ studio/components/SidePanel.jsx
@@ -57,7 +57,7 @@ const SidePanel = () => {
     }
 
     return (
-        <div style={{ width: "100%", minWidth: "350px", height: '100vh', backgroundColor: "#FCFCFC", boxShadow: "0px 0px 32px 0px rgba(0,0,0,0.2)" }}>
+        <div style={{ width: "100%", minWidth: "393px", height: '100vh', backgroundColor: "#FCFCFC", boxShadow: "0px 0px 32px 0px rgba(0,0,0,0.2)" }}>
             <div style={{ display: "flex", justifyContent: "space-between", flexDirection: "column", height: "100%" }}>
                 <InspectorHeader />
                 <div style={{ backgroundColor: "#fff", padding: "0 12px 0 12px" }}>
