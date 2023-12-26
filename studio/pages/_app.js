d88b2805dc4e4fca05e5fae0ceaf5c354407efbe
Fredi Bach <fredi.bach@unic.com>
2023-12-26 19:28:46 +0100

    Improve context menu and tab menu styles

--- studio/pages/_app.js
+++ studio/pages/_app.js
@@ -2,9 +2,10 @@ import { PrimeReactProvider } from 'primereact/api';
 import App from '../components/App';
 import '../styles.css';
 
-import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 import "primeflex/primeflex.css";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 import "primeflex/themes/primeone-light.css";
+import "../public/studio-styles.css";
 
 const MyApp = ({ Component, pageProps }) => {
     const value = {};
