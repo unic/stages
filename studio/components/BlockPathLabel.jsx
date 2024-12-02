e4db68d83f81fe4e0dcaacf39cfa5092ceb2b6a0
Fredi Bach <fredi.bach@unic.com>
2024-12-02 15:42:38 +0100

    Refactor BlockPathLabel to use styled components

--- studio/components/BlockPathLabel.jsx
+++ studio/components/BlockPathLabel.jsx
@@ -1,102 +1,119 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from "react";
 import sanitizeHtml from "sanitize-html";
-import useStagesStore from './store';
+import styled from "@emotion/styled";
+import useStagesStore from "./store";
+import SizeButton from "./SizeButton";
+import EditablePath from "./EditablePath";
 
-const SizeButton = ({ size, isActive, type, onChangeBlockWidth }) => {
-    return <button type="button" style={{
-        border: "1px solid #ddd",
-        borderColor: type === "fieldset" ? isActive ? "#c10b99" : "#bbb" : isActive ? "#0A94F8" : "#bbb",
-        background: "#fff",
-        borderRadius: "3px",
-        cursor: "pointer",
-        userSelect: "none",
-        fontSize: "10px",
-        color: type === "fieldset" ? isActive ? "#c10b99" : "#bbb" : isActive ? "#0A94F8" : "#bbb",
-        margin: "0 0 1px 0",
-        padding: "0 1px",
-        width: "14px",
-        textAlign: "center"
-    }} onClick={(e) => {
-        e.preventDefault();
-        if (onChangeBlockWidth) onChangeBlockWidth(size);
-    }}>{size}</button>;
-};
+const PathContainer = styled.span`
+  display: inline-block;
+  position: absolute;
+  top: calc(100% / 2 - 24px);
+  left: -14px;
+  font-size: 11px;
+  color: ${(props) =>
+    props.isHovered
+      ? props.type === "fieldset"
+        ? "#c10b99"
+        : "#0A94F8"
+      : "#ccc"};
+  background: transparent;
+  padding: 1px 4px;
+  border-radius: 3px;
+  user-select: none;
+  white-space: nowrap;
+  max-width: 100%;
+  overflow: hidden;
 
-const BlockPathLabel = ({ path, inCollection, isHovered, type, fieldsetId, blockWidth, onChangeBlockWidth, setFormCounter }) => {
+  & > div {
+    display: inline-block;
+    borderradius: 3px;
+  }
+`;
+
+const BlockPathLabel = ({
+  path,
+  inCollection,
+  isHovered,
+  type,
+  fieldsetId,
+  blockWidth,
+  onChangeBlockWidth,
+  setFormCounter,
+}) => {
+  const indexOfLastPathDot = path.lastIndexOf(".");
+  const store = useStagesStore();
+  const [editablePath, setEditablePath] = useState(
+    indexOfLastPathDot === -1 ? path : path.substring(indexOfLastPathDot + 1)
+  );
+  const [nonEditablePath, setNonEditablePath] = useState(
+    indexOfLastPathDot === -1 ? "" : path.substring(0, indexOfLastPathDot + 1)
+  );
+
+  useEffect(() => {
     const indexOfLastPathDot = path.lastIndexOf(".");
-    const store = useStagesStore();
-    const [editablePath, setEditablePath] = useState(indexOfLastPathDot === -1 ? path : path.substring(indexOfLastPathDot + 1));
-    const [nonEditablePath, setNonEditablePath] = useState(indexOfLastPathDot === -1 ? "" : path.substring(0, indexOfLastPathDot + 1));
+    setEditablePath(
+      indexOfLastPathDot === -1 ? path : path.substring(indexOfLastPathDot + 1)
+    );
+    setNonEditablePath(
+      indexOfLastPathDot === -1 ? "" : path.substring(0, indexOfLastPathDot + 1)
+    );
+  }, [path]);
 
-    useEffect(() => {
-        const indexOfLastPathDot = path.lastIndexOf(".");
-        setEditablePath(indexOfLastPathDot === -1 ? path : path.substring(indexOfLastPathDot + 1));
-        setNonEditablePath(indexOfLastPathDot === -1 ? "" : path.substring(0, indexOfLastPathDot + 1));
-    }, [path]);
+  const handleEditPath = useCallback((evt) => {
+    const sanitizeConf = {
+      allowedTags: [],
+      allowedAttributes: {},
+    };
+    const newEditablePath = sanitizeHtml(
+      evt.currentTarget.innerHTML,
+      sanitizeConf
+    ).replace(/\s/g, "X");
+    setEditablePath(newEditablePath);
+    store.onUpdatePath(nonEditablePath, editablePath, newEditablePath);
+    if (typeof setFormCounter === "function")
+      setFormCounter((formCounter) => formCounter + 1);
+  }, []);
 
-    const handleEditPath = useCallback(evt => {
-        const sanitizeConf = {
-            allowedTags: [],
-            allowedAttributes: {}
-        };
-        const newEditablePath = sanitizeHtml(evt.currentTarget.innerHTML, sanitizeConf).replace(/\s/g, "X");
-        setEditablePath(newEditablePath);
-        store.onUpdatePath(nonEditablePath, editablePath, newEditablePath);
-        if (typeof setFormCounter === "function") setFormCounter(formCounter => formCounter + 1);
-    }, []);
-
-    return (
-        <>
-            <span style={{
-                display: "inline-block",
-                position: "absolute",
-                top: inCollection ? "-13px" : "-9px",
-                right: "6px",
-                fontSize: "11px",
-                color: isHovered ? type === "fieldset" ? "#c10b99" : "#0A94F8" : "#ccc",
-                background: "#fff",
-                padding: "1px 4px",
-                borderRadius: "3px",
-                userSelect: "none",
-                whiteSpace: "nowrap",
-                maxWidth: "100%",
-                overflow: "hidden"
-            }}>
-                {type === "fieldset" ? `{${fieldsetId}} ` : null}
-                {type === "group" ? "[••] " : null}{type === "collection" ? "[=] " : null}
-                {type === "wizard" ? "[ : ] " : null}
-                {type === "stage" ? "[ . ] " : null}
-                {type === "field" ? "[ ] " : null}
-                {nonEditablePath}<span contentEditable dangerouslySetInnerHTML={{__html: editablePath}} onClick={(e) => e.preventDefault()} onBlur={handleEditPath} />
-            </span>
-            {isHovered && (
-                <span style={{
-                    display: "inline-block",
-                    position: "absolute",
-                    top: "calc(100% / 2 - 24px)",
-                    left: "-14px",
-                    fontSize: "11px",
-                    color: isHovered ? type === "fieldset" ? "#c10b99" : "#0A94F8" : "#ccc",
-                    background: "transparent",
-                    padding: "1px 4px",
-                    borderRadius: "3px",
-                    userSelect: "none",
-                    whiteSpace: "nowrap",
-                    maxWidth: "100%",
-                    overflow: "hidden"
-                }}>
-                    <div style={{ display: "inline-block", borderRadius: "3px" }}>
-                        <SizeButton isActive={blockWidth === "small"} size="S" type={type} onChangeBlockWidth={onChangeBlockWidth} />
-                        <br />
-                        <SizeButton isActive={blockWidth === "medium"} size="M" type={type} onChangeBlockWidth={onChangeBlockWidth} />
-                        <br />
-                        <SizeButton isActive={!blockWidth || blockWidth === "large"} size="L" type={type} onChangeBlockWidth={onChangeBlockWidth} />
-                    </div>
-                </span>
-            )}
-            
-        </>
-    );  
+  return (
+    <>
+      <EditablePath
+        inCollection={inCollection}
+        isHovered={isHovered}
+        type={type}
+        fieldsetId={fieldsetId}
+        nonEditablePath={nonEditablePath}
+        editablePath={editablePath}
+        handleEditPath={handleEditPath}
+      />
+      {isHovered && (
+        <PathContainer isHovered={isHovered} type={type}>
+          <div>
+            <SizeButton
+              isActive={blockWidth === "small"}
+              size="S"
+              type={type}
+              onChangeBlockWidth={onChangeBlockWidth}
+            />
+            <br />
+            <SizeButton
+              isActive={blockWidth === "medium"}
+              size="M"
+              type={type}
+              onChangeBlockWidth={onChangeBlockWidth}
+            />
+            <br />
+            <SizeButton
+              isActive={!blockWidth || blockWidth === "large"}
+              size="L"
+              type={type}
+              onChangeBlockWidth={onChangeBlockWidth}
+            />
+          </div>
+        </PathContainer>
+      )}
+    </>
+  );
 };
 
 export default BlockPathLabel;
