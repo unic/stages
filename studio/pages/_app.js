a07aa1b51a32b42a14b6247e52e3de5a2d2684f5
Fredi Bach <fredi.bach@unic.com>
2024-12-03 11:02:03 +0100

    Fix theme import

--- studio/pages/_app.js
+++ studio/pages/_app.js
@@ -2,9 +2,9 @@ import { PrimeReactProvider } from 'primereact/api';
 import App from '../components/App';
 import '../styles/globals.css';
 
+import "../node_modules/primereact/resources/themes/lara-light-cyan/theme.css";
 import "../node_modules/primeflex/primeflex.css";
 import "../node_modules/primeflex/themes/primeone-light.css";
-import "../node_modules/primereact/resources/themes/lara-light-cyan/theme.css";
 
 const MyApp = ({ Component, pageProps }) => {
     const value = {};
